0c20b2cbd81752fc7791efb3f44406cbed3f5669
Vincent <23420208+vincentneo@users.noreply.github.com>
2021-04-27 11:46:34 +0800

    section b stuff

--- app/src/main/java/sg/edu/rp/c347/id19007966/demoexplicitintent/HeroStatsActivity.java
+++ app/src/main/java/sg/edu/rp/c347/id19007966/demoexplicitintent/HeroStatsActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -28,5 +29,27 @@ public class HeroStatsActivity extends AppCompatActivity {
         tvStrength.setText("Strength: " + hero.getStrength());
         tvTechnicalProwess.setText("Technical: " + hero.getTechnicalProwess());
 
+        btnLike = findViewById(R.id.buttonLike);
+        btnDislike = findViewById(R.id.buttonDislike);
+
+        btnLike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonDidClickWithLike(true);
+            }
+        });
+        btnDislike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonDidClickWithLike(false);
+            }
+        });
+    }
+
+    private void buttonDidClickWithLike(boolean like) {
+        Intent i = new Intent();
+        i.putExtra("like", like ? "like" : "dislike");
+        setResult(RESULT_OK, i);
+        finish();
     }
 }
--- app/src/main/java/sg/edu/rp/c347/id19007966/demoexplicitintent/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/id19007966/demoexplicitintent/MainActivity.java
@@ -1,14 +1,18 @@
 package sg.edu.rp.c347.id19007966.demoexplicitintent;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    int requestCodeForSupermanStats = 1;
+    int requestCodeForBatmanStats = 2;
     TextView tvSuperman, tvBatman;
 
     @Override
@@ -22,22 +26,42 @@ public class MainActivity extends AppCompatActivity {
         tvSuperman.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                goToActivity(new Hero("Superman", 100, 60));
+                goToActivity(new Hero("Superman", 100, 60),
+                        requestCodeForSupermanStats);
             }
         });
 
         tvBatman.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                goToActivity(new Hero("Batman", 60, 90));
+                goToActivity(new Hero("Batman", 60, 90),
+                        requestCodeForBatmanStats);
             }
         });
     }
 
-    private void goToActivity(Hero hero) {
+    private void goToActivity(Hero hero, int requestCode) {
         Intent i = new Intent(MainActivity.this, HeroStatsActivity.class);
         i.putExtra("hero", hero);
-        startActivity(i);
+        startActivityForResult(i, requestCode);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && data != null) {
+            String like = data.getStringExtra("like");
+            String statement = "";
+
+            if (requestCode == requestCodeForSupermanStats) {
+                statement = "You " + like + " Superman";
+            }
+
+            if (requestCode == requestCodeForBatmanStats) {
+                statement = "You " + like + " Batman";
+            }
+
+            Toast.makeText(MainActivity.this, statement, Toast.LENGTH_LONG).show();
+        }
+    }
 }
